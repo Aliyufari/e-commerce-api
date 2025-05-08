ba0bdad42119796593ecbfd5cf3cfbbba653a7a4
Aliyu Abubakar <aliyufari@gmail.com>
2025-05-08 22:17:31 +0100

    Updated Swagger Documentation

--- src/config/swagger.ts
+++ src/config/swagger.ts
@@ -1,36 +1,36 @@
 // @ts-ignore
 import swaggerJSDoc from 'swagger-jsdoc';
 
-const options: swaggerJSDoc.options = {
-    definition:{
-        openapi: '3.0.0',
-        info: {
-            title: 'e-commerce App API',
-            version: '1.0.0',
-            description: 'An e-commerce application API using Express with TypeScript'
-        },
-        servers: [
-            {
-                url: `http://localhost:${process.env.PORT}`
-            }
-        ]
-    },
-    components: {
+const options: swaggerJSDoc.Options = {
+    definition: {
+      openapi: '3.0.0',
+      info: {
+        title: 'e-commerce App API',
+        version: '1.0.0',
+        description: 'An e-commerce application API using Express with TypeScript'
+      },
+      servers: [
+        {
+          url: `http://localhost:${process.env.PORT}`
+        }
+      ],
+      components: { // ✅ Move this inside 'definition'
         schemas: {
           User: {
             type: 'object',
             properties: {
               name: { type: 'string' },
               email: { type: 'string', format: 'email' },
-              gender: { type: 'string', format: 'gender' },
-              phone: { type: 'string', format: 'phone' },
+              gender: { type: 'string' },
+              phone: { type: 'string' },
               password: { type: 'string', format: 'password' },
             },
             required: ['name', 'email', 'password'],
           },
         },
+      },
     },
     apis: ['**/routes/*.ts'],
-}
+};  
 
 export const swaggerSpec = swaggerJSDoc(options);
