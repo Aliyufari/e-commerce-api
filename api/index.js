9bde2a1883578e6d5767cb0af26782dadbf75286
Aliyu Abubakar <aliyufari@gmail.com>
2025-05-08 14:28:02 +0100

    Added Vercel Config for Deployment

--- api/index.js
+++ api/index.js
@@ -1,4 +1,9 @@
-import app from '../dist/src/index.js';
+import { App } from '../src/App';
 import serverless from 'serverless-http';
 
+// Initialize app instance
+const appInstance = new App();
+const app = appInstance.getApp();
+
+// Export the serverless handler
 export default serverless(app);
